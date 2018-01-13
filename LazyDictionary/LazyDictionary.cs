a20941bb20c4608147794a4eb05a54d87d37cc44
Philemon Eichin <phileicht@gmx.de>
2018-01-13 23:22:50 +0100

    Update LazyDictionary

--- LazyDictionary/LazyDictionary.cs
+++ LazyDictionary/LazyDictionary.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices.ComTypes;
 
-namespace phirSOFT
+namespace phirSOFT.LazyDictionary
 {
+    /// <inheritdoc />
     /// <summary>
-    /// Provides a <see cref="IDictionary{TKey,TValue}"/> that will create missing values when requested.
+    ///     Provides a <see cref="T:System.Collections.Generic.IDictionary`2" /> that will create missing values when requested.
     /// </summary>
     /// <typeparam name="TKey">The type of the key</typeparam>
     /// <typeparam name="TValue">The type of the value</typeparam>
@@ -15,23 +15,29 @@ namespace phirSOFT
         private readonly IDictionary<TKey, TValue> _dictionary;
         private readonly Func<TKey, TValue> _generatorFunc;
 
+
+        /// <inheritdoc />
         /// <summary>
-        /// Creates a new Lazy dictionary with an genearator function.
+        ///     Creates a new Lazy dictionary with an genearator function.
         /// </summary>
         /// <param name="generatorFunc">The function used to generate miisng members</param>
         public LazyDictionary(Func<TKey, TValue> generatorFunc) : this(generatorFunc, new Dictionary<TKey, TValue>())
-        { }
+        {
+        }
 
+        /// <inheritdoc />
         /// <summary>
-        /// Creates a new Lazy dictionary with an genearator function.
+        ///     Creates a new Lazy dictionary with an genearator function.
         /// </summary>
         /// <param name="generatorFunc">The function used to generate miisng members</param>
         /// <param name="comparer">The comparer that should be used for comparing the keys.</param>
-        public LazyDictionary(Func<TKey,TValue> generatorFunc, IEqualityComparer<TKey> comparer) : this(generatorFunc, new Dictionary<TKey, TValue>(comparer))
-        { }
+        public LazyDictionary(Func<TKey, TValue> generatorFunc, IEqualityComparer<TKey> comparer) : this(generatorFunc,
+            new Dictionary<TKey, TValue>(comparer))
+        {
+        }
 
         /// <summary>
-        /// Creates a new Lazy dictionary with an genearator function.
+        ///     Creates a new Lazy dictionary with an genearator function.
         /// </summary>
         /// <param name="generatorFunc">The function used to generate miisng members</param>
         /// <param name="dictionary">The dictionary that should be used for storing.</param>
@@ -39,92 +45,92 @@ namespace phirSOFT
         public LazyDictionary(Func<TKey, TValue> generatorFunc, IDictionary<TKey, TValue> dictionary)
         {
             if (dictionary == null) throw new ArgumentNullException(nameof(dictionary));
-            if(dictionary.IsReadOnly)
-                throw new ArgumentException("Invalid Dictionary. The dictionary must not be readonly", nameof(dictionary));
+            if (dictionary.IsReadOnly)
+                throw new ArgumentException("Invalid Dictionary. The dictionary must not be readonly",
+                    nameof(dictionary));
 
             _generatorFunc = generatorFunc ?? throw new ArgumentNullException(nameof(generatorFunc));
             _dictionary = dictionary;
         }
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
             return _dictionary.GetEnumerator();
         }
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
+        
         IEnumerator IEnumerable.GetEnumerator()
         {
             return ((IEnumerable) _dictionary).GetEnumerator();
         }
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
+        
         public void Add(KeyValuePair<TKey, TValue> item)
         {
             _dictionary.Add(item);
         }
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
+        
         public void Clear()
         {
             _dictionary.Clear();
         }
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
+        
+        /// <inheritdoc />
         /// <remarks>This value determs, wheter the value is allready in the dictionary. This will not invoke the geenrator</remarks>
         public bool Contains(KeyValuePair<TKey, TValue> item)
         {
             return _dictionary.Contains(item);
         }
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
+        
         public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
         {
             _dictionary.CopyTo(array, arrayIndex);
         }
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
+        
         public bool Remove(KeyValuePair<TKey, TValue> item)
         {
             return _dictionary.Remove(item);
         }
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
+        
         public int Count => _dictionary.Count;
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
+        
         public bool IsReadOnly => _dictionary.IsReadOnly;
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
+        
         public void Add(TKey key, TValue value)
         {
             _dictionary.Add(key, value);
         }
 
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
+        
         public bool ContainsKey(TKey key)
         {
             return _dictionary.ContainsKey(key);
         }
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
+        
         public bool Remove(TKey key)
         {
             return _dictionary.Remove(key);
         }
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
+        
         public bool TryGetValue(TKey key, out TValue value)
         {
             if (!_dictionary.TryGetValue(key, out value))
-            {
                 value = GenerateEntry(key);
-            }
             return true;
         }
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
+        
         public TValue this[TKey key]
         {
             get
@@ -132,20 +138,14 @@ namespace phirSOFT
                 TryGetValue(key, out var retval);
                 return retval;
             }
-            set { _dictionary[key] = value; }
+            set => _dictionary[key] = value;
         }
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
-        public ICollection<TKey> Keys
-        {
-            get { return _dictionary.Keys; }
-        }
+        
+        public ICollection<TKey> Keys => _dictionary.Keys;
 
-        /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
-        public ICollection<TValue> Values
-        {
-            get { return _dictionary.Values; }
-        }
+        
+        public ICollection<TValue> Values => _dictionary.Values;
 
         private TValue GenerateEntry(TKey key)
         {
---
+++ LazyDictionary/WeakLazyDictionary.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace phirSOFT.LazyDictionary
+{
+    /// <inheritdoc />
+    /// <summary>
+    ///     Provides a <see cref="T:System.Collections.Generic.IDictionary`2" /> that will create missing values when requested,
+    ///     but will not keep a strong reference to it.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key</typeparam>
+    /// <typeparam name="TValue">The type of the value</typeparam>
+    public class WeakLazyDictionary<TKey, TValue> : IDictionary<TKey, TValue> where TValue : class
+    {
+        private readonly Dictionary<TKey, WeakReference<TValue>> _dictionary =
+            new Dictionary<TKey, WeakReference<TValue>>();
+
+        private readonly Func<TKey, TValue> _generatorFunc;
+
+        public WeakLazyDictionary(Func<TKey, TValue> generatorFunc)
+        {
+            _generatorFunc = generatorFunc;
+        }
+
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return _dictionary.Select(kv =>
+                {
+                    var exists = kv.Value.TryGetTarget(out var value);
+                    return new {kv.Key, exists, value};
+                })
+                .Where(p => p.exists)
+                .Select(p => new KeyValuePair<TKey, TValue>(p.Key, p.value))
+                .GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            return Contains(item) && _dictionary.Remove(item.Key);
+        }
+
+        public int Count => _dictionary.Count;
+
+        public bool IsReadOnly => true;
+
+        public void Add(TKey key, TValue value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return _dictionary.ContainsKey(key);
+        }
+
+        public bool Remove(TKey key)
+        {
+            return _dictionary.Remove(key);
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            if (_dictionary.ContainsKey(key))
+            {
+                _dictionary[key].TryGetTarget(out value);
+                return true;
+            }
+            value = null;
+            return false;
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (_dictionary.TryGetValue(key, out var reference) &&
+                    reference.TryGetTarget(out var value))
+                    return value;
+                value = _generatorFunc(key);
+                _dictionary[key] = new WeakReference<TValue>(value);
+                return value;
+            }
+            set => throw new NotSupportedException();
+        }
+
+        public ICollection<TKey> Keys => _dictionary.Keys;
+
+        public ICollection<TValue> Values => _dictionary.Values
+            .Select(reference =>
+            {
+                var test = reference.TryGetTarget(out var value);
+                return new {test, value};
+            })
+            .Where(t => t.test)
+            .Select(t => t.value)
+            .ToList();
+    }
+}
