0e00c325943be6c86a48c3731d8f17f3e1c1aa13
Philemon Eichin <phileicht@gmx.de>
2018-02-26 11:25:00 +0100

    Cleanup

--- LazyDictionary/MnDictionary.cs
+++ LazyDictionary/MnDictionary.cs
@@ -2,28 +2,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace phirSOFT.LazyDictionary
 {
+    /// <inheritdoc cref="IDictionary{TKey,TValue}"/>
     /// <summary>
-    /// Provides a dictionary that can have multiple values associated with one key.
+    ///     Provides a dictionary that can have multiple values associated with one key.
     /// </summary>
     /// <typeparam name="TKey">The type of the key</typeparam>
     /// <typeparam name="TValue">The type of the values</typeparam>
     public class MnDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IReadOnlyDictionary<TKey, IEnumerable<TValue>>
     {
         private readonly Dictionary<TKey, LinkedList<TValue>> _store = new Dictionary<TKey, LinkedList<TValue>>();
-        private int _count;
-
-        IEnumerator<KeyValuePair<TKey, IEnumerable<TValue>>> IEnumerable<KeyValuePair<TKey, IEnumerable<TValue>>>.GetEnumerator()
-        {
-            return _store.Select(kv => new KeyValuePair<TKey, IEnumerable<TValue>>(kv.Key, kv.Value)).GetEnumerator();
-        }
 
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
-            return _store.SelectMany(k => k.Value.Select(v => new KeyValuePair<TKey, TValue>(k.Key, v))).GetEnumerator();
+            return _store.SelectMany(k => k.Value.Select(v => new KeyValuePair<TKey, TValue>(k.Key, v)))
+                .GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
@@ -39,7 +34,7 @@ namespace phirSOFT.LazyDictionary
         public void Clear()
         {
             _store.Clear();
-            _count = 0;
+            Count = 0;
         }
 
         public bool Contains(KeyValuePair<TKey, TValue> item)
@@ -51,10 +46,7 @@ namespace phirSOFT.LazyDictionary
         {
             using (var pairs = GetEnumerator())
             {
-                while (arrayIndex < array.Length && pairs.MoveNext())
-                {
-                    array[arrayIndex++] = pairs.Current;
-                }
+                while (arrayIndex < array.Length && pairs.MoveNext()) array[arrayIndex++] = pairs.Current;
             }
         }
 
@@ -63,13 +55,12 @@ namespace phirSOFT.LazyDictionary
             if (!_store.TryGetValue(item.Key, out var list) || !list.Remove(item.Value)) return false;
             if (list.Count == 0)
                 _store.Remove(item.Key);
-            _count--;
+            Count--;
             return true;
-
-
         }
 
-        public int Count => _count;
+        public int Count { get; private set; }
+
         public bool IsReadOnly => false;
 
         public void Add(TKey key, TValue value)
@@ -77,12 +68,43 @@ namespace phirSOFT.LazyDictionary
             if (!_store.ContainsKey(key))
                 _store.Add(key, new LinkedList<TValue>());
             _store[key].AddLast(value);
-            _count++;
+            Count++;
         }
 
         public bool ContainsKey(TKey key)
         {
             return _store.ContainsKey(key);
+        }
+
+
+        public bool Remove(TKey key)
+        {
+            if (!_store.TryGetValue(key, out var list)) return false;
+            Count -= list.Count;
+            _store.Remove(key);
+            return true;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            var result = _store.TryGetValue(key, out var list);
+            value = result ? list.First.Value : default(TValue);
+            return result;
+        }
+
+        public TValue this[TKey key]
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public ICollection<TKey> Keys => _store.Keys;
+        public ICollection<TValue> Values => _store.Values.SelectMany(c => c).ToList().AsReadOnly();
+
+        IEnumerator<KeyValuePair<TKey, IEnumerable<TValue>>> IEnumerable<KeyValuePair<TKey, IEnumerable<TValue>>>.
+            GetEnumerator()
+        {
+            return _store.Select(kv => new KeyValuePair<TKey, IEnumerable<TValue>>(kv.Key, kv.Value)).GetEnumerator();
         }
 
         public bool TryGetValue(TKey key, out IEnumerable<TValue> value)
@@ -97,32 +119,5 @@ namespace phirSOFT.LazyDictionary
         IEnumerable<TKey> IReadOnlyDictionary<TKey, IEnumerable<TValue>>.Keys => Keys;
 
         IEnumerable<IEnumerable<TValue>> IReadOnlyDictionary<TKey, IEnumerable<TValue>>.Values => _store.Values;
-
-
-
-        public bool Remove(TKey key)
-        {
-            if (!_store.TryGetValue(key, out var list)) return false;
-            _count -= list.Count;
-            _store.Remove(key);
-            return true;
-
-        }
-
-        public bool TryGetValue(TKey key, out TValue value)
-        {
-            var result = _store.TryGetValue(key, out var list);
-            value = result ? list.First.Value : default(TValue);
-            return result;
-        }
-
-        public TValue this[TKey key]
-        {
-            get { throw new NotImplementedException(); }
-            set { throw new NotImplementedException(); }
-        }
-
-        public ICollection<TKey> Keys => _store.Keys;
-        public ICollection<TValue> Values => _store.Values.SelectMany(c => c).ToList().AsReadOnly();
     }
 }
